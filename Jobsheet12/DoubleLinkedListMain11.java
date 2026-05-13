bfa00fe2c1b5d3ab44342bbc95174a5a52217a80
melizahaiziK <melizaha1zik@gmail.com>
2026-05-13 15:34:55 +0700

    modfi percobaan 1

--- Jobsheet12/DoubleLinkedListMain11.java
+++ Jobsheet12/DoubleLinkedListMain11.java
@@ -14,6 +14,7 @@ public class DoubleLinkedListMain11 {
             System.out.println("4. Hapus data di awal");
             System.out.println("5. Hapus data di akhir");
             System.out.println("6. Tampilkan data");
+            System.out.println("7. Tampilkan data terbalik");
             System.out.println("0. Keluar");
             System.out.print("Pilih menu : ");
             pilihan = sc.nextInt();
@@ -43,6 +44,9 @@ public class DoubleLinkedListMain11 {
                     break;
                 case 6:
                     list.print();
+                    break;
+                case 7:
+                    list.printReverse();
                     break;
                 case 0:
                     System.out.println("Program selesai.");
